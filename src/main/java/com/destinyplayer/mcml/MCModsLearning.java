bdb11986687fdf1822f49754a5668844a00a41bc
DestinyPlayer <thenextdecade244@gmail.com>
2015-08-03 17:19:05 +0300

    Main Class added

---
+++ src/main/java/com/destinyplayer/mcml/MCModsLearning.java
@@ -0,0 +1,34 @@
+package com.destinyplayer.mcml;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by DestinyPlayer on 03.08.2015.
+ */
+@Mod(modid = "MCModsLearning", name = "Minecraft Mods Learning", version = "1.7.10-0.1")
+public class MCModsLearning
+{
+    @Mod.Instance("MCModsLearning")
+    public static MCModsLearning instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
